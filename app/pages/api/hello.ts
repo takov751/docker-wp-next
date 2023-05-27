0fb74db7fdc1be0fc020f82db292226757257413
Tom Barber <tom.barber@webandroll.co.uk>
2023-05-27 14:00:34 +1000

    Test an API route - All good in standalone mode

---
+++ app/pages/api/hello.ts
@@ -0,0 +1,10 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> {
+  res.status(200).json({
+    message: "Hello World",
+  });
+}
