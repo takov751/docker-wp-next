799b04995d79a148e6ee9c26730989d1bc55f26b
Tom Barber <tom.barber@webandroll.co.uk>
2023-05-28 08:33:21 +1000

    rename bedrock to cms. Add APP_DOMAIN

--- bin/create-certs.sh
+++ bin/create-certs.sh
@@ -11,8 +11,11 @@ source .env
 # create directory for certificates
 mkdir -p nginx/certs
 
-mkcert -install "${DOMAIN}"
+mkcert -install "${CMS_DOMAIN}"
+mkcert -install "${APP_DOMAIN}"
 
 # Move the generated certificate and key to the certs directory
-mv "${DOMAIN}.pem" nginx/certs/
-mv "${DOMAIN}-key.pem" nginx/certs/
+mv "${CMS_DOMAIN}.pem" nginx/certs/
+mv "${CMS_DOMAIN}-key.pem" nginx/certs/
+mv "${APP_DOMAIN}.pem" nginx/certs/
+mv "${APP_DOMAIN}-key.pem" nginx/certs/
