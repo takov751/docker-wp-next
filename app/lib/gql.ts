db1f2d3ebf7197ccd2c3a1e259190152184646e3
takov751 <takov751@tutanota.com>
2023-06-06 03:32:01 +0100

    fixed nginx and docker network and added mysql user

--- app/lib/gql.ts
+++ app/lib/gql.ts
@@ -1,3 +1,7 @@
 import { GraphQLClient } from "graphql-request";
-
-export const gqlClient = new GraphQLClient("http://cms:9000/wp/graphql");
+// this https rejectUnauthorized changes only needed if the domain name is self signed and you want to use https inside docker network. 
+//If the domain is public and has dns challanged singed certification this is not needed.
+//import https from 'https';
+//export const httpsAgent = new https.Agent({ rejectUnauthorized: false });
+//export const gqlClient = new GraphQLClient("https://cms.example.test/wp/graphql", { agent: httpsAgent });
+export const gqlClient = new GraphQLClient("http://cms.example.test:8080/wp/graphql");
